2700c21f11be3ce5603fa898e692900dbeb111e9
IsMe Nang <48167845+nangsks@users.noreply.github.com>
2025-03-06 13:59:50 +0700

    add

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,20 +1,12 @@
 import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {Locale, routing} from '@/i18n/routing';
+
  
-export default async function LocaleLayout({
-  children,
-  params
-}: {
+type Props = {
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
-}) {
-  // Ensure that the incoming `locale` is valid
-  const {locale} = await params;
-  if (!routing.locales.includes(locale as Locale)) {
-    notFound();
+  params: {locale: string};
   }
+export default async function Layout({ children, params:{ locale }}: Props){
  
   // Providing all messages to the client
   // side is the easiest way to get started
